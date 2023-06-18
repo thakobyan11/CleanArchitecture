c9faa2f9b4ccd7cb9d29e0860b604ab036b90ae6
thakobyan11 <thakobyan11@gmail.com>
2023-06-18 17:19:20 +0400

    impl_data

---
+++ app/src/main/java/com/practics/practics/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package com.practics.practics.data
+
+import com.practics.practics.domain.ShopItem
+import com.practics.practics.domain.ShopListRepository
+
+class ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == -1) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    // will be changed
+    override fun editShopItem(shopItem: ShopItem) {
+        deleteShopItem(getShopItemById(shopItem.id))
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItemById(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> = shopList
+}
--- app/src/main/java/com/practics/practics/domain/ShopItem.kt
+++ app/src/main/java/com/practics/practics/domain/ShopItem.kt
@@ -1,7 +1,7 @@
 package com.practics.practics.domain
 
 data class ShopItem(
-    val id:Int,
+    var id:Int = -1,
     val name:String,
     val count:Int,
     val isActive:Boolean
