4bbc8b62c492963a43e7cce9f71f8441cbe93209
thakobyan11 <thakobyan11@gmail.com>
2023-06-28 21:30:49 +0400

    create_adapter

--- app/src/main/java/com/practics/practics/presentation/MainActivity.kt
+++ app/src/main/java/com/practics/practics/presentation/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.practics.practics.presentation
 
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.practics.practics.R
-import com.practics.practics.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var linearLayout: LinearLayout
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var shopListAdapter: ShopListAdapter
 
     private lateinit var vm: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,14 +22,14 @@ class MainActivity : AppCompatActivity() {
         linearLayout = findViewById(R.id.linear_layout)
 
         vm.listShop.observe(this) {
-            adapter.shopList = it
+            shopListAdapter.submitList(it)
         }
     }
 
     private fun setUpAdapter() {
-        adapter = ShopListAdapter()
+        shopListAdapter = ShopListAdapter()
         val rcv: RecyclerView = findViewById(R.id.rcv)
-        rcv.adapter = adapter
+        rcv.adapter = shopListAdapter
         rcv.recycledViewPool.setMaxRecycledViews(
             ShopListAdapter.VIEW_TYPE_IS_ACTIVE,
             ShopListAdapter.MAX_POOL_COUNT
@@ -40,5 +38,38 @@ class MainActivity : AppCompatActivity() {
             ShopListAdapter.VIEW_TYPE_IS_NOT_ACTIVE,
             ShopListAdapter.MAX_POOL_COUNT
         )
+        setUpLongClick()
+        setUpOnClick()
+        val callBack = object :
+            ItemTouchHelper.SimpleCallback(
+                0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = shopListAdapter.currentList[viewHolder.adapterPosition]
+                vm.deleteShopItem(item)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(callBack)
+        itemTouchHelper.attachToRecyclerView(rcv)
+    }
+
+    private fun setUpOnClick() {
+        shopListAdapter.shopItemClickListener = {
+            Log.d("TAG", "CLICK")
+        }
+    }
+
+    private fun setUpLongClick() {
+        shopListAdapter.shopItemLongClickListener = {
+            vm.changeActiveState(it)
+        }
     }
 }
