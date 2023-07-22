0658bb53dc571292506ec984205f6f2cad768857
thakobyan11 <thakobyan11@gmail.com>
2023-07-22 14:39:03 +0400

    impl_DB

---
+++ app/src/main/java/com/practics/practics/data/ShopListMapper.kt
@@ -0,0 +1,24 @@
+package com.practics.practics.data
+
+import com.practics.practics.domain.ShopItem
+
+class ShopListMapper {
+
+    fun mapEntityToDbModel(shopItem: ShopItem) = ShopItemDB(
+        id = shopItem.id,
+        name = shopItem.name,
+        count = shopItem.count,
+        isActive = shopItem.isActive
+    )
+
+    fun mapDbModelToEntity(shopItemDB: ShopItemDB) = ShopItem(
+        id = shopItemDB.id,
+        name = shopItemDB.name,
+        count = shopItemDB.count,
+        isActive = shopItemDB.isActive
+    )
+
+    fun mapListDbModelToListEntity(list:List<ShopItemDB>) = list.forEach {
+        mapDbModelToEntity(it)
+    }
+}
